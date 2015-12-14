74bf3f2c86c61219a712995588658a7a020bd172
Orochena, Eduardo <eduardo.orochena@risk.lexisnexis.com>
2015-12-14 10:44:49 -0500

    initial commit for bash version of RoxieAdmin

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo cp ~/Foxie/Bash-Complete/Foxie /etc/bash_completion.d/
+sudo cp ~/Foxie/Bash-Complete/auto.sh /etc/profile.d/
+sudo cp ~/Foxie/bin/Foxie /usr/bin/
+sudo cp ~/Foxie/ManPages/foxie.8.gz /usr/share/man/man8/
